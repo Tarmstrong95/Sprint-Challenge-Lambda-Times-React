7f5cdad55a2cbe3024024e70fc8950a71a223cc0
Tarmstrong95 <triston95strong@gmail.com>
2019-05-03 14:24:12 -0500

    Add Header styled components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import {MainHeader, Date, Temp, HeaderH1} from './StyledComponents'
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <MainHeader className="header">
+      <Date className="date">SMARCH 32, 2018</Date>
+      <HeaderH1>Lambda Times</HeaderH1>
+      <Temp className="temp">98°</Temp>
+    </MainHeader>
   )
 }
 
--- lambdatimes/src/components/StyledComponents.js
+++ lambdatimes/src/components/StyledComponents.js
@@ -1,13 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export const Header
+//========= header =========
+export const MainHeader = styled.div`
+display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+
+`
+export const HeaderSpan = styled.span`
+align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`
+export const Date = styled(HeaderSpan)`
+margin-left: 25px;
+  flex: 1;
+`
+export const HeaderH1 = styled.h1`
+font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`
+export const Temp = styled(HeaderSpan)`
+text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`
+
+//======= Top Bar ======= 
 
 
 
 
+const Dummy = () => null
 
-
-
-
-
-export default Dummy = () => {};
+export default Dummy
