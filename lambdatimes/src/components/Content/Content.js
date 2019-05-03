eb6cac3938ca386c66c24ede5fff4f12d0fd242d
Tarmstrong95 <triston95strong@gmail.com>
2019-05-03 13:47:35 -0500

    Add card toggle functionality

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -33,7 +33,7 @@ export default class Content extends Component {
     })
   };
 
-  filterCards = (e) => {
+  filterCards = () => {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
@@ -46,11 +46,11 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-     return this.state.cards
-
-  //  this.state.selected === 'all' ? 
-  //  this.state.cards : 
-  //  this.state.cards.filter(card => card.tab === this.state.selected)
+     if(this.state.selected === 'all'){
+       return this.state.cards
+     }else {
+       return this.state.cards.filter(card => card.tab === this.state.selected)
+     }
   };
 
   render() {
